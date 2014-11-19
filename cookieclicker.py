48fc827bba60c6d2e59c4151525f1f6afab97c6b
atwachtm <atwachtm@dhcp-09-26.bravo>
2014-11-19 12:32:09 -0600

    Updated with the imports

--- cookieclicker.py
+++ cookieclicker.py
@@ -32,7 +32,6 @@ class Cookieclicker(unittest.TestCase):
         print total_cookies
         print Cookie_string
         print numCookies"""
-<<<<<<< HEAD
         file_import = False
         product_count = 0
         ach_count = 0
@@ -63,18 +62,9 @@ class Cookieclicker(unittest.TestCase):
                 if self.is_element_present(By.ID, "promptOption1"):
                     driver.find_element_by_id("promptOption1").click()
                     driver.find_element_by_id("promptOption0").click()
-=======
-        if self.is_element_present(By.ID, "bakeryName"):
-            Bake_Name = driver.find_element_by_id("bakeryName")
-            Bake_Name.click()
-            if self.is_element_present(By.ID, "promptOption1"):
-                driver.find_element_by_id("promptOption1").click()
-                driver.find_element_by_id("promptOption0").click()
->>>>>>> FETCH_HEAD
         #Products
         product_button0 = driver.find_element_by_id("product0")
         cursor_count = 0
-        print str(cursor_count) + " cursors have been bought"
         #Grandma
         product_button1 = driver.find_element_by_id("product1")
         grandma_count1 = 0
@@ -105,7 +95,6 @@ class Cookieclicker(unittest.TestCase):
         #Prism
         product_button10 = driver.find_element_by_id("product10")
         prism_count10 = 0
-<<<<<<< HEAD
         if file_import is True:
             cursor_count = driver.find_element_by_xpath("//div[@id='product0']/div[3]/div[3]").text
             grandma_count1 = driver.find_element_by_xpath("//div[@id='product1']/div[3]/div[3]").text
@@ -162,38 +151,6 @@ class Cookieclicker(unittest.TestCase):
                 stats.click()
             #Buying products
             if "enabled" in product_button0.get_attribute('class') and cursor_count < cursor_max:
-=======
-        #Upgrades
-        #Upgrade_list = [100, 500, 1000, 5000, 10000, 50000, 100000, 500000, 1000000, 5000000]
-        #Upgrade_list2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
-        stats = driver.find_element_by_id('statsButton')
-        stats.click()
-        Small_cookie = driver.find_element_by_class_name("tinyCookie")
-        Small_cookie.click()
-        ach_count = 0
-        product_count = 0
-        click_count = 0
-        while product_count < 10:
-            driver.find_element_by_id("bigCookie").click()
-            click_count += 1
-            """Cookie_string = driver.find_element_by_id("cookies").text
-            Cookie_list = Cookie_string.split(" ")
-            Avail_cookies = str(Cookie_list[0])
-            print Cookie_list[0]
-            print Avail_cookies
-            for i in Avail_cookies:
-                if i == ',':
-                    y = Avail_cookies.replace(',', "")
-                    #print y
-                    Cookie_list.insert(0, y)
-                elif i == '.':
-                    z = Avail_cookies.replace('.', "")
-                    Cookie_list.insert(0, z)"""
-                    #print Cookie_list
-            #numCookies = float(Cookie_list[0])
-            #Buying products Round 1
-            if "enabled" in product_button0.get_attribute('class') and cursor_count < 10:
->>>>>>> FETCH_HEAD
                 product_button0.click()
                 cursor_count += 1
                 product_count += 1
@@ -233,7 +190,6 @@ class Cookieclicker(unittest.TestCase):
                 product_button9.click()
                 antimatter_count9 += 1
                 product_count += 1
-<<<<<<< HEAD
             if "enabled" in product_button10.get_attribute('class') and prism_count10 < other_max:
                 product_button10.click()
                 prism_count10 += 1
@@ -242,12 +198,6 @@ class Cookieclicker(unittest.TestCase):
                 cursor_max = 400
                 grandma_max = 250
                 other_max = 200
-=======
-            if "enabled" in product_button10.get_attribute('class') and prism_count10 < 10:
-                product_button10.click()
-                prism_count10 += 1
-                product_count += 1
->>>>>>> FETCH_HEAD
             """
             #Selling
             if grandma_count1 == 11:
@@ -275,70 +225,6 @@ class Cookieclicker(unittest.TestCase):
                     ach_count += 1
             except (StaleElementReferenceException, NoSuchElementException):
                 print "Caught stale element--skippin'!"
-        while product_count >= 10 and product_count <= 2500:
-            driver.find_element_by_id("bigCookie").click()
-            if "enabled" in product_button0.get_attribute('class') and cursor_count < 400:
-                product_button0.click()
-                cursor_count += 1
-                product_count += 1
-            if "enabled" in product_button1.get_attribute('class') and grandma_count1 < 250:
-                product_button1.click()
-                grandma_count1 += 1
-                product_count += 1
-            if "enabled" in product_button2.get_attribute('class') and farm_count2 < 200:
-                product_button2.click()
-                farm_count2 += 1
-                product_count += 1
-            if "enabled" in product_button3.get_attribute('class') and factory_count3 < 200:
-                product_button3.click()
-                factory_count3 += 1
-                product_count += 1
-            if "enabled" in product_button4.get_attribute('class') and mine_count4 < 200:
-                product_button4.click()
-                mine_count4 += 1
-                product_count += 1
-            if "enabled" in product_button5.get_attribute('class') and shipment_count5 < 200:
-                product_button5.click()
-                shipment_count5 += 1
-                product_count += 1
-            if "enabled" in product_button6.get_attribute('class') and alchemy_count6 < 200:
-                product_button6.click()
-                alchemy_count6 += 1
-                product_count += 1
-            if "enabled" in product_button7.get_attribute('class') and portal_count7 < 200:
-                product_button7.click()
-                portal_count7 += 1
-                product_count += 1
-            if "enabled" in product_button8.get_attribute('class') and tmachine_count8 < 200:
-                product_button8.click()
-                tmachine_count8 += 1
-                product_count += 1
-            if "enabled" in product_button9.get_attribute('class') and antimatter_count9 < 200:
-                product_button9.click()
-                antimatter_count9 += 1
-                product_count += 1
-            if "enabled" in product_button10.get_attribute('class') and prism_count10 < 200:
-                product_button10.click()
-                prism_count10 += 1
-                product_count += 1
-            if self.is_element_present(By.XPATH, "//div[@id='upgrades']/div[contains(@class, 'enabled')]"):
-                print "Checking for Upgrades... "
-                driver.find_element_by_xpath("//div[@id='upgrades']/div[contains(@class, 'upgrade enabled')]").click()
-            #perform the export
-            if product_count % 10 == 0:
-                self.Export_save()
-            try:
-                #total number of achievements
-                ach_string = driver.find_element_by_xpath("//div[@id='menu']/div[5]/div[2]").text
-                ach_list = ach_string.split(' ')
-                ach_string2 = ach_list[2]
-                ach_list2 = ach_string2.split('/')
-                #clean up of achievements
-                if ach_count < int(ach_list2[0]):
-                    driver.find_element_by_xpath("//div[contains(@class, 'note')]/div[contains(@class, 'close')]").click()
-                    ach_count += 1
-            except (StaleElementReferenceException, NoSuchElementException):
-                print "Caught stale element--skippin'!"
 
     """def handler(signum, frame):
         menu_save = self.driver.find_element_by_id('prefsButton')
@@ -348,22 +234,6 @@ class Cookieclicker(unittest.TestCase):
             export_code = self.driver.find_element_by_xpath("//div[@id='promptContent']/div[2]/textarea").text
             save_data.write(str(export_code))
             menu_save.click()
-        self.driver.find_element_by_id("promptOption0").click()
-        raise KeyboardInterrupt"""
-
-<<<<<<< HEAD
-    """def handler(signum, frame):
-=======
-    def Export_save(self):
->>>>>>> FETCH_HEAD
-        menu_save = self.driver.find_element_by_id('prefsButton')
-        menu_save.click()
-        self.driver.find_element_by_xpath("//div[@id='menu']/div[3]/div[3]/a").click()
-        with open("exportSave.txt", "w+") as save_data:
-            export_code = self.driver.find_element_by_xpath("//div[@id='promptContent']/div[2]/textarea").text
-            save_data.write(str(export_code))
-            menu_save.click()
-<<<<<<< HEAD
         self.driver.find_element_by_id("promptOption0").click()
         raise KeyboardInterrupt"""
 
@@ -380,12 +250,6 @@ class Cookieclicker(unittest.TestCase):
 
     def Import_save(self):
         print "Importing data. Please Wait..."
-=======
-            self.driver.find_element_by_id("promptOption0").click()
-        print "An export occurred but may not be what you wanted, hur hur hur!"
-
-    def Import_save(self):
->>>>>>> FETCH_HEAD
         menu_save = self.driver.find_element_by_id('prefsButton')
         menu_save.click()
         self.driver.find_element_by_xpath("//div[@id='menu']/div[3]/div[3]/a[2]").click()
@@ -393,10 +257,7 @@ class Cookieclicker(unittest.TestCase):
             import_box = self.driver.find_element_by_xpath("//div[@id='promptContent']/div[2]/textarea")
             save_code = import_data.read()
             import_box.send_keys(save_code)
-<<<<<<< HEAD
             self.driver.find_element_by_id("promptOption0").click()
-=======
->>>>>>> FETCH_HEAD
             menu_save.click()
 
     def is_element_present(self, how, what):
